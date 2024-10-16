69e10d1c19fd266dced8db29f5f827343636f953
metshein <metshein@gmail.com>
2024-10-16 11:46:29 +0300

    Update yl04a.py

--- yl04a.py
+++ yl04a.py
@@ -1,3 +1,18 @@
 # 16.10.24 Metshein
 # Ülesanded 04
 
+
+#2. Raamatute allahindlus
+ale = 0.3
+kogus = 3
+hind = 12.53
+summa = hind - (hind * ale) * kogus
+print(f"{kogus} raamatu hind soodustusega on {summa:0.2f}€")
+
+
+
+# 1. Aia pikkus
+a = int(input("Sisesta külg 1: "))
+b = int(input("Sisesta külg 2: "))
+p = 2 * (a + b)
+print(f"Aia kogupikkus on {p} meetrit.")
