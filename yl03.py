88bd901f125b8304f0eb0dbae635cb5969ff7c90
metshein <metshein@gmail.com>
2024-10-16 11:25:57 +0300

    Create yl03.py

---
+++ yl03.py
@@ -0,0 +1,38 @@
+#16.10.24 Metshein
+#Ülesanded 03
+#import turtle
+
+
+#Ülesanne 3.2: Ostunimekiri
+
+#Ülesanne 3.4: Lemmikraamat
+
+
+
+#Ülesanne 3.5: Unistuste auto
+mark, mudel = "bmw", "118"
+auto = mark+" "+mudel
+aasta = 1997
+hind = 301.60
+
+print("Minu unistuste auto on",auto,"aastast",aasta,", mille hind on",hind,"eurot.")
+
+
+"""
+#Ülesanne 3.7: Python Turtle kolmnurk
+kylje_pikkus = 200
+nurk = 120
+kujundi_varv = "red"
+
+turtle.color(kujundi_varv)
+
+for i in range(3):
+    for i in range(3):
+        turtle.forward(kylje_pikkus)
+        turtle.left(nurk)
+
+    turtle.penup()
+    turtle.forward(kylje_pikkus*1.5)
+    turtle.pendown()
+"""
+#turtle.done()
