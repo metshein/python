341a0e84e9b75fac8358e648b767b2d77a15eec2
metshein <metshein@gmail.com>
2024-10-18 10:56:43 +0300

    Update yl04a.py

--- yl04a.py
+++ yl04a.py
@@ -1,5 +1,28 @@
 # 16.10.24 Metshein
 # Ülesanded 04
+import turtle
+
+# 5. Ringi pindala ja Turtle
+try:
+    r = int(input("Lisa ringi raadius: "))
+    pi  = 3.14
+    s = pi*r**2 #astendamine
+    p = 2*pi*r
+    print(f"Ringi pindala on {s:.2f} ja ümbermõõt on {p:.2f} ")
+    turtle.circle(r, 360)
+except:
+    print("Tegid sisestamisel vea!")
+
+# 4. Kingituste pakkimine
+try:
+    kingitused = int(input("Lisa kinkide arv: "))
+    maht = 5
+    pakid = kingitused // maht
+    yle = kingitused % maht
+    print(f"Saad teha {pakid} täis kinkekasti. Üle jääb {yle} kingitust.")
+except:
+    print("Tegid sisestamisel vea!")
+
 
 
 # 3. Faili allalaadimine
@@ -25,3 +48,5 @@ a = int(input("Sisesta külg 1: "))
 b = int(input("Sisesta külg 2: "))
 p = 2 * (a + b)
 print(f"Aia kogupikkus on {p} meetrit.")
+
+turtle.done()
