0e617eee105f2c44d44e75f7b144a197bb11ab06
metshein <metshein@gmail.com>
2024-10-18 11:51:22 +0300

    sdfdsfg

--- yl03.py
+++ yl03.py
@@ -1,40 +1,17 @@
-#16.10.24 Metshein
-#Ülesanded 03
-#import turtle
+import math
 
+# Andmed
+korgus = 4.4  # meetrit
+nurk = math.radians(53)  # teisendame kraadid radiaanideks
 
-#Ülesanne 3.2: Ostunimekiri
+# Kauguse arvutamine tangensfunktsiooni abil
+kaugus = korgus / math.tan(nurk)
 
+# Redeli pikkuse arvutamine Pythagorase teoreemi abil
+pikkus = math.sqrt(korgus**2 + kaugus**2)
 
+# Ümardame vastused kahe komakohani
+kaugus_2_komakohta = round(kaugus, 2)
+pikkus_2_komakohta = round(pikkus, 2)
 
-#Ülesanne 3.4: Lemmikraamat
-
-
-
-#Ülesanne 3.5: Unistuste auto
-mark, mudel = "bmw", "118"
-auto = mark+" "+mudel
-aasta = 1997
-hind = 301.60
-
-print("Minu unistuste auto on",auto,"aastast",aasta,", mille hind on",hind,"eurot.")
-
-
-"""
-#Ülesanne 3.7: Python Turtle kolmnurk
-kylje_pikkus = 200
-nurk = 120
-kujundi_varv = "red"
-
-turtle.color(kujundi_varv)
-
-for i in range(3):
-    for i in range(3):
-        turtle.forward(kylje_pikkus)
-        turtle.left(nurk)
-
-    turtle.penup()
-    turtle.forward(kylje_pikkus*1.5)
-    turtle.pendown()
-"""
-#turtle.done()
+print(kaugus_2_komakohta, pikkus_2_komakohta)
---
+++ yl06a.py
@@ -0,0 +1,25 @@
+# 18.10.24 Metshein
+# Ülesanded 6
+
+import turtle
+import math
+
+kordaja = 10
+a = math.radians(53)
+h = 4.4
+x = abs(h / math.tan(a))
+c = math.sqrt(h**2 + x ** 2)
+
+print(x)
+
+turtle.fd(x*kordaja)
+turtle.lt(90)
+turtle.fd(h*kordaja)
+turtle.lt(180-(180-90-53))
+turtle.fd(c*kordaja)
+
+
+
+
+turtle.done()
+
