d50e51e7690633b628f902217039bf4451952f42
metshein <metshein@gmail.com>
2024-12-05 15:13:03 +0200

    Update 02.py

---
+++ 02.py
@@ -0,0 +1,57 @@
+# 2. Ülesanne
+# 05.12.24
+import turtle
+"""
+    Loo värvilised olümpiarõngad vastavalt nõutele
+    Loo aken, mille nimi on “Olümpiarõngad ja sinu nimi”
+    Akna suurus 500×400
+    Loo värvilised 50px olümpiarõngad (sinine, must, punane, kollane, roheline)
+    Joone paksus 6
+    Kiirus 0
+"""
+
+turtle.speed(0)
+#sinine
+turtle.penup()
+turtle.goto(-200,100)
+turtle.pendown()
+turtle.color("blue")
+turtle.pensize(6)
+turtle.circle(50,360)
+#must
+turtle.penup()
+turtle.goto(-90,100)
+turtle.pendown()
+turtle.color("black")
+turtle.pensize(6)
+turtle.circle(50,360)
+#punane
+turtle.penup()
+turtle.goto(20,100)
+turtle.pendown()
+turtle.color("red")
+turtle.pensize(6)
+turtle.circle(50,360)
+#kollane
+turtle.penup()
+turtle.goto(-150,50)
+turtle.pendown()
+turtle.color("yellow")
+turtle.pensize(6)
+turtle.circle(50,360)
+#roheline
+turtle.penup()
+turtle.goto(-30,50)
+turtle.pendown()
+turtle.color("green")
+turtle.pensize(6)
+turtle.circle(50,360)
+
+
+
+
+
+
+
+
+turtle.done()
