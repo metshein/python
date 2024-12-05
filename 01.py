1862b8d1d83a2d0ea0eb54fda8e45a09478f28f2
metshein <metshein@gmail.com>
2024-12-05 14:51:55 +0200

    Update 01.py

--- 01.py
+++ 01.py
@@ -5,6 +5,7 @@
 # see impordib kilpkonna mooduli
 import turtle
 
+#kolmnurk
 turtle.speed(0) # reguleeri 1-9
 turtle.penup()
 turtle.goto(-500,200)
@@ -15,6 +16,16 @@ turtle.forward(200) #fd, pikslites
 turtle.left(120) #lt, rt
 turtle.forward(200) #fd, pikslites
 
+#süda
+turtle.penup()
+turtle.goto(-200,200)
+turtle.pendown()
+turtle.left(120) #lt, rt
+turtle.fd(100)
+turtle.circle(50,180)
+turtle.right(90)
+turtle.circle(50,180)
+turtle.fd(100)
 
 # lõpetab kilpkonna, et ei jookseks kokku 
 turtle.done()
