b99d4a5d2e7dfe5556f3898a4bb39d15ce7393b6
metshein <metshein@gmail.com>
2024-12-05 14:38:52 +0200

    <saagradfgadfg

---
+++ 01.py
@@ -0,0 +1,21 @@
+
+# 01. ülesanne
+# Metshein 05.12.24
+
+# see impordib kilpkonna mooduli
+import turtle
+
+turtle.speed(0) # reguleeri 1-9
+turtle.penup()
+turtle.goto(-500,200)
+turtle.pendown()
+turtle.forward(200) #fd, pikslites
+turtle.left(120) #lt, rt
+turtle.forward(200) #fd, pikslites
+turtle.left(120) #lt, rt
+turtle.forward(200) #fd, pikslites
+
+
+# lõpetab kilpkonna, et ei jookseks kokku 
+turtle.done()
+
